fa311974712f790b53183434b27d931f349e27e6
mps <panneerselvam.mani@superops.ai>
2024-07-17 16:15:41 +0530

    navbar added in syncro page

--- src/Syncro.jsx
+++ src/Syncro.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./HomePageComponent/Navbar"
 import Box from "./SyncroPageComponent/ReviewBox"
 import SwitchBox from "./SyncroPageComponent/SwitchBox"
-
 
 function Syncro() {
   return (
     <>
+    <Navbar/>
     <Box/>
     <SwitchBox/>
     </>
